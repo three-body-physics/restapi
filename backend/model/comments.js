eab1511d5a1204374de3a2cce983e6e258f7d226
three-body-physics <mintpie@googlemail.com>
2017-10-31 09:00:41 +0000

    changed comment.js

--- backend/model/comments.js
+++ backend/model/comments.js
@@ -1,13 +1,13 @@
 var mongoose = require("mongoose");
 
 module.exports = new mongoose.Schema({
-  author: {id: {
-  				type: mongoose.Schema.Types.ObjectId,
-  				ref: "User"
-  			   },
-			username: String			
-		},
-  comment: String,
-  date: String
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comment: String,
+    date: String
 });
-
