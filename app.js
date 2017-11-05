bfd70591a7161394b4fadffa49e13ea8ba8813f3
three-body-physics <mintpie@googlemail.com>
2017-11-05 11:04:48 +0000

    added cors

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ var bodyParser = require("body-parser");
 var port = process.env.PORT || 3000
 var passport = require("passport");
 
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//       res.send(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(passport.initialize());
 
 
